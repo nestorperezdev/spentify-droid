398c098c6768695fa22134735ffd6fe7733282d5
NestorPerez <nolvera@expediagroup.com>
2024-05-15 18:11:25 -0600

    Centered loading spinner.

--- app/expenses/src/main/java/com/nestor/expenses/ui/expenselist/ExpenseListScreen.kt
+++ app/expenses/src/main/java/com/nestor/expenses/ui/expenselist/ExpenseListScreen.kt
@@ -1,6 +1,7 @@
 package com.nestor.expenses.ui.expenselist
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -10,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -50,7 +52,9 @@ private fun ExpenseListContent(
     ) {
         if (expenseList.isLoading) {
             item {
-                CircularProgressIndicator()
+                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator()
+                }
             }
         }
         expenseList.body?.let {
