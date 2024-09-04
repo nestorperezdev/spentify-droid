d658206b01688d91ebfcd7b1cf5de3b2c9b88666
Néstor Pérez <111082591+nperez96@users.noreply.github.com>
2024-09-04 11:19:29 -0600

    Moved category picker to his own dialog

--- app/expenses/src/main/java/com/nestor/expenses/ui/NewExpenseScreen.kt
+++ app/expenses/src/main/java/com/nestor/expenses/ui/NewExpenseScreen.kt
@@ -7,18 +7,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -32,13 +26,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalTextInputService
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -48,11 +39,11 @@ import com.nestor.database.data.catergory.CategoryEntity
 import com.nestor.database.data.currency.CurrencyEntity
 import com.nestor.database.data.subcategory.SubcategoryWithCategories
 import com.nestor.expenses.R
+import com.nestor.expenses.ui.category.CategoryPickerSheet
 import com.nestor.schema.utils.ResponseWrapper
 import com.nestor.uikit.SpentifyTheme
 import com.nestor.uikit.button.SYButton
 import com.nestor.uikit.input.FormFieldData
-import com.nestor.uikit.list.SYItemIcon
 import com.nestor.uikit.list.SYListItem
 import com.nestor.uikit.list.SYListItemData
 import com.nestor.uikit.loading.LoadingScreen
@@ -225,91 +216,6 @@ private fun NewExpenseScreenContent(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun CategoryPickerSheet(
-    sheetState: SheetState = rememberModalBottomSheetState(),
-    categories: StateFlow<ResponseWrapper<List<SubcategoryWithCategories>>>,
-    onCategorySelected: (CategoryEntity) -> Unit,
-    onDismissRequest: () -> Unit
-) {
-    val coroutineScope = rememberCoroutineScope()
-    ModalBottomSheet(onDismissRequest = onDismissRequest, sheetState = sheetState) {
-        CategoryPickerDialogContent(
-            modifier = Modifier.padding(LocalSYPadding.current.screenHorizontalPadding),
-            categories = categories.collectAsState().value,
-            onCategorySelected = {
-                onCategorySelected(it)
-                coroutineScope.launch { sheetState.hide() }
-                onDismissRequest()
-            }
-        )
-    }
-}
-
-@Composable
-private fun CategoryPickerDialogContent(
-    modifier: Modifier = Modifier,
-    categories: ResponseWrapper<List<SubcategoryWithCategories>>,
-    onCategorySelected: (CategoryEntity) -> Unit,
-) {
-    Column(
-        modifier = modifier.verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        if (categories.isLoading) {
-            Text(text = "Loading")
-        }
-        categories.body?.let {
-            it.forEach { subcat ->
-                Text(text = subcat.subCategory.name)
-                LazyRow {
-                    val categoryEntities = subcat.categories
-                    items(categoryEntities.size) { index ->
-                        val cat = categoryEntities[index]
-                        CategoryView(
-                            item = cat,
-                            modifier = Modifier
-                                .width(95.dp)
-                                .clickable { onCategorySelected(cat) },
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun CategoryView(
-    modifier: Modifier = Modifier,
-    item: CategoryEntity
-) {
-    val painter =
-        rememberAsyncImagePainter(model = item.iconUrl(LocalSYImageServerProvider.current))
-    val tint = if (item.tint != null) Color(item.tint!!) else MaterialTheme.colorScheme.primary
-    val icon = remember {
-        SYListItemData.SYListItemIcon(tint = tint, icon = painter)
-    }
-    Column(
-        modifier = modifier.padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        SYItemIcon(
-            icon = icon
-        )
-        Text(
-            text = item.name,
-            style = MaterialTheme.typography.labelSmall,
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
-        )
-    }
-}
-
 @Composable
 private fun NewExpenseToolbar(onNavBack: () -> Unit) {
     SYStatusBar(
@@ -335,19 +241,5 @@ private fun NewExpenseScreenContentPreview() {
             onCategorySelected = {},
             categorySelected = MutableStateFlow(null)
         )
-    }
-}
-
-@Preview
-@Composable
-private fun CategoryPickerSheetPreview() {
-    SpentifyTheme {
-        Scaffold {
-            CategoryPickerDialogContent(
-                modifier = Modifier.padding(it),
-                categories = ResponseWrapper.loading(),
-                onCategorySelected = {}
-            )
-        }
     }
 }
